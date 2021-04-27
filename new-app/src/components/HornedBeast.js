a820f1fa1dce8ec1bf616b20d622a5bd6fb52573
Sukina AbuHammad <sukina_89@yahoo.com>
2021-04-27 15:40:09 +0300

    Edit

--- new-app/src/components/HornedBeast.js
+++ new-app/src/components/HornedBeast.js
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+
 
 
 class HornedBeasts extends React.Component {
@@ -20,7 +20,7 @@ class HornedBeasts extends React.Component {
     
     render (){
        return (
-        <Card style={{ width: '30rem',margin:'3rem 37rem 3rem 37rem', color:'#0064ED' , textAlign:'center', border:'2px solid #00ADBE'}}>
+        <Card style={{ width: '30rem', color:'#0064ED' , textAlign:'center', border:'2px solid #00ADBE'}}>
         <Card.Img  src={this.props.image_url} />
         <Card.Body>
         <Card.Title>{this.props.title}</Card.Title>
